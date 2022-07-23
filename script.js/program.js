a7d2d1341008a07bff4c98adb03700f39901fb2f
Your Name <107193947+MohamedaliHammi@users.noreply.github.com>
2022-07-23 13:10:24 +0100

    first part of zeyed tut

--- script.js/program.js
+++ script.js/program.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+console.log('hello world');
+
+ /*let firstNumber =   parseInt (prompt  ('Enter your first number'));
+let secondNumber =  parseInt (prompt ('Enter your second number'));
+let result = firstNumber + secondNumber; 
+
+alert(result);
+
+console.log(typeof firstNumber); 
+*/
+let name = prompt('enter your name');
+let age = prompt ('enter your age');
+let adj = prompt('enter an adjective');
+let favGame = prompt('what is your favorite game');
+
+alert(`Hello Mr. ${name} you are ${age} and you look ${adj} today, do you really enjoy playing ${favGame}`);
+
